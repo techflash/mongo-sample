f5c859a01b233b1787e5d53fd6b1840517002453
Sunil Vishwakarma <svi@marketlogicsoftware.com>
2020-05-16 12:59:44 +0200

    added test

--- src/main/java/com/svi/mongo/documents/Department.java
+++ src/main/java/com/svi/mongo/documents/Department.java
@@ -1,11 +1,12 @@
 package com.svi.mongo.documents;
 
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-
+@Builder
 @Document(value = Department.DOCUMENT_NAME)
 public class Department {
 
--- src/test/java/com/svi/mongo/MongoApplicationTests.java
+++ src/test/java/com/svi/mongo/MongoApplicationTests.java
@@ -1,13 +1,38 @@
 package com.svi.mongo;
 
+import com.svi.mongo.documents.Department;
+import com.svi.mongo.repository.DepartmentRepository;
+import com.svi.mongo.service.DepartmentService;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
 
-//@SpringBootTest
-//class MongoApplicationTests {
-//
-//  @Test
-//  void contextLoads() {
-//  }
-//
-//}
+class MongoApplicationTests {
+
+  DepartmentService sut;
+  DepartmentRepository departmentRepository;
+
+  @BeforeEach
+  void setup() {
+    departmentRepository = Mockito.mock(DepartmentRepository.class);
+    sut = new DepartmentService(departmentRepository);
+  }
+
+  @Test
+  void shouldGetAllDepartments() {
+    // given
+    Mockito.when(departmentRepository.findAll()).thenReturn(
+        Arrays.asList(Department.builder().name("IT").id("1001").location("Berlin").build())
+    );
+
+    // when
+    List<Department> departmentList = sut.getAllDepartment();
+
+    Assertions.assertEquals(departmentList.get(0).getName()
+        , "IT");
+  }
+
+}
